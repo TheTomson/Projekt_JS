80c905b1013953fdb14e46428acf086a12af6ceb
TheTomson <33695195+TheTomson@users.noreply.github.com>
2020-10-04 11:21:54 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//Zadanie 1
+
+console.log("Tomasz");
+
+//Zadanie 2
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//Zadanie 3
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
+
+//w pkt 1 poprawnie
+//w pkt 2 pojawia się błąd bo nie ma takiej zmiennej
+//w pkt 3 pojawia się bład
