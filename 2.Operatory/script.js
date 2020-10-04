024be40810eb2d9cc6ede26a1fe57f1ffb5290b6
TheTomson <33695195+TheTomson@users.noreply.github.com>
2020-10-04 11:32:27 +0200

    Create script.js

---
+++ 2.Operatory/script.js
@@ -0,0 +1,53 @@
+//Zadanie 1
+
+var value1 = false;
+var value2 = false;
+
+console.log(value1 == value2);
+
+//Zadanie 2
+
+var number1 = 2;
+var number2 = 5;
+var moduloResult = 0;
+moduloResult = number2 % number1;
+console.log(moduloResult);
+
+//Zadanie 3
+
+let word1 = "Pies";
+let word2 = "Kot";
+
+let stringResult = "";
+
+stringResult = word1 + word2;
+
+console.log(stringResult);
+
+//Zadanie 4:
+
+var someNumber =425;
+var someString ="425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+//  W pierszym porównanie jest wartości 
+//  W drugim porówanie jest wartości jak i typu zmiennej
+
+// Zadanie 5:
+
+var counter = 30;
+console.log(counter);//30
+counter++;
+console.log(counter);//31
+counter--;
+console.log(counter);//30
+
+//Zadanie 6:
+
+let somevar1 = 1;
+let somevar2 = 2;
+let result = null;
+result = somevar1 > somevar2;
+console.log(result);
