bf396959167e6075d9732633a93a5b9f620ad43d
TheTomson <33695195+TheTomson@users.noreply.github.com>
2021-01-31 10:56:10 +0100

    Create script.js

---
+++ 2.Zmienne i Stałe/script.js
@@ -0,0 +1,39 @@
+//zadanie 1
+const num = 1,
+    str = '2',
+    combo = num + str,
+    bool = true,
+    nothing = null;
+
+console.log(num);
+console.log(str);
+console.log(combo);
+console.log(bool);
+console.log(nothing);
+
+
+//zadanie 2
+
+    num = 1,
+    num1 = 2,
+    sum = 0;
+
+sum = num + num1;
+
+console.log(sum);
+
+// zadanie 3
+
+let num;
+console.log(num); // brak przypisanej wartości do zmiennej
+
+let sum = 100 ;
+
+for ( i = 100; i < 250; i ++);
+{
+    if (i%2 === 0)
+    console.log(i);
+    else
+    console.log('jakas chujnia');
+
+}
